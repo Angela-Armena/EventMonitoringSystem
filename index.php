ea6b72d9b27ce30750c053e839ffd6cad30d6e2f
Angela-Armena <126683068+Angela-Armena@users.noreply.github.com>
2023-12-05 00:39:47 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,224 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Web NFC Sample</title>
+    <link rel="stylesheet" href="attendance.css">
+</head>
+<style>
+    body {
+    background-color: #ffffff;
+    box-sizing: border-box;
+    font-family: "Roboto", "Helvetica", "Arial", sans-serif;
+    }
+    
+    @media screen and (min-width: 832px) {
+    body {
+        width: 800px;
+        margin: 0 auto;
+    }
+    }
+
+    h1 {
+    margin-bottom: -0.3em;
+    }
+
+    h2 {
+    margin-top: 2em;
+    }
+
+    h3 {
+    margin-bottom: -0.2em;
+    margin-top: 2em;
+    }
+
+    .pageIcon {
+    height: 2.3em;
+    float: left;
+    margin-right: 0.5em;
+    }
+
+    .availability {
+    margin-bottom: 2em;
+    }
+
+    .output {
+    background-color: #f0f0f0;
+    border-radius: 0.75em;
+    display: block;
+    margin: 0.5em;
+    padding: 0.5em;
+    }
+
+    #log {
+    margin: .5em 0;
+    white-space: pre-wrap;
+    }
+
+    #status:empty,
+    #log:empty,
+    #content:empty {
+    display: none;
+    }
+
+    .highlight {
+    border-radius: 0.75em;
+    border: 1px solid #f0f0f0;
+    display: block;
+    margin: 0.5em;
+    overflow-x: auto;
+    padding: 0.5em;
+    }
+
+    code {
+    font-family: Inconsolata, Consolas, monospace;
+    }
+</style>
+
+<body>
+
+    <h1>Web NFC Sample</h1>
+    <br><br><br><br><br>
+
+    <button id="scanButton">Scan</button>
+
+    <!-- DISPLAYS ERROR IF THE NFC ATTENDANCE SCANNER IS OPENED IN A COMPUTER AND NOT A PHONE -->
+    <script>
+        var ChromeSamples = {
+            log: function () {
+                var line = Array.prototype.slice.call(arguments).map(function (argument) {
+                    return typeof argument === 'string' ? argument : JSON.stringify(argument);
+                }).join(' ');
+
+                document.querySelector('#log').textContent += line + '\n';
+            },
+
+            clearLog: function () {
+                document.querySelector('#log').textContent = '';
+            },
+
+            setStatus: function (status) {
+                document.querySelector('#status').textContent = status;
+            }
+        };
+    </script>
+
+    <h2 id="status"></h2>
+
+    <h3>Live Output</h3>
+    <div id="output" class="output">
+        <pre id="log"></pre>
+    </div>
+
+    <!-- DISPLAYS ERROR IF THE NFC ATTENDANCE SCANNER DOES NOT WORK ON THE CURRENT VERSION OF CHROME -->
+    <script>
+        if (/Chrome\/(\d+\.\d+.\d+.\d+)/.test(navigator.userAgent)) {
+            if (89 > parseInt(RegExp.$1)) {
+                ChromeSamples.setStatus('Warning! Keep in mind this sample has been tested with Chrome ' + 89 + '.');
+            }
+        }
+    </script>
+
+    <!-- DISPLAYS ERROR IF THE NFC ATTENDANCE SCANNER IS OPENED ON A DESKTOP COMPUTER AND NOT A PHONE -->
+    <script>
+        log = ChromeSamples.log;
+
+        if (!("NDEFReader" in window))
+            ChromeSamples.setStatus("Web NFC is not available. Use Chrome on Android.");
+    </script>
+
+    <!-- STARTS SCAN WHEN BUTTON IS CLICKED -->
+    <script>
+        let scanning = false;
+
+        scanButton.addEventListener("click", async () => {
+            if (scanning) return; // Prevent multiple scans
+            
+            log("Scanning for ID...");
+            scanning = true;
+
+            try {
+                const ndef = new NDEFReader();
+                let scanTimeout = setTimeout(() => {
+                    if (scanning) {
+                        log("Nothing has been scanned. Please try again.");
+                        stopScan();
+                    }
+                }, 5000); // 5 seconds timeout
+
+                await ndef.scan();
+                log("> Scan started");
+
+                ndef.addEventListener("readingerror", () => {
+                    log("Cannot read data from the NFC tag. Please try again.");
+                    stopScan();
+                });
+
+                ndef.addEventListener("reading", ({ message, serialNumber }) => {
+                    log(`> Serial Number: ${serialNumber}`);
+                    // log(`> Records: (${message.records.length})`);
+
+                    fetch('test.php', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify({ serialNumber: serialNumber }),
+                    })
+                    .catch(error => {
+                        console.error('Error: ', error);
+                    });
+
+                    stopScan();
+                });
+            } catch (error) {
+                log("Argh! " + error);
+                stopScan();
+            }
+        });
+
+        function stopScan() {
+            scanning = false;
+            clearTimeout(scanTimeout);
+        }
+    </script>
+
+    <?php
+    // Establish connection to the database (replace these with your credentials)
+    $HOSTNAME = "adu-ems.mysql.database.azure.com";
+    $USERNAME = "MA2O";
+    $PASSWORD = "Mamao...";
+    $DATABASE = "event-management-system";
+
+    // Create connection
+    $conn = new mysqli($HOSTNAME, $USERNAME, $PASSWORD, $DATABASE);
+
+    // Check connection
+    if ($conn -> connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // Check if a serial number was received from the frontend
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $data = json_decode(file_get_contents("php://input"));
+        if (isset($data -> serialNumber)) {
+            $serialNumber = $data -> serialNumber;
+
+            // Insert the serial number into the database
+            $query = "INSERT INTO requests (serialNumber) VALUES ('$serialNumber')";
+
+            if ($conn -> query($query) === TRUE) {
+                echo "Serial number inserted successfully";
+            } else {
+                echo "Error: " . $query . "<br>" . $conn -> error;
+            }
+        } else {
+            echo "Serial number not received";
+        }
+    }
+
+    $conn->close();
+    ?>
+</body>
+
+</html>
