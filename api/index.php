6651cea49319dacc5edc9362e4bab9d07ec112df
Angela Armena <marie.angela.armena499@adamson.edu.ph>
2023-12-10 17:26:33 +0800

    1

--- api/index.php
+++ api/index.php
@@ -6,7 +6,7 @@
 </head>
 <style>
     body {
-    background-color: darksalmon;
+    background-color: darkseagreen;
     width: 100%;
     padding: 0;
     margin: 0;
@@ -53,11 +53,11 @@
     }
 
     .scanContainer {
-        width: 100%;
-        display: block;
-        justify-content: center;
-        align-items: center;
-        background-color: green;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: green;
     }
 
     #scanButton {
